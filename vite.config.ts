c0e8e94ca1a3d5e058492817b543bbea41a26bd4
thorettonedwin-slatedigital <edwin.thoretton@slatedigital.com>
2025-11-20 17:50:51 +0100

    setup project

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import {defineConfig} from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from "@react-router/dev/vite";
+
+
+export default defineConfig({
+    plugins: [tsconfigPaths(), reactRouter()]
+})
